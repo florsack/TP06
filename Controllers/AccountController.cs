ea96dbd99ca2f3cc83d086f2143f0d4ce63e7cf3
Flor <49628016@est.ort.edu.ar>
2025-08-05 12:15:30 -0300

    Creamos la base de datos

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,8 @@
+/*
+Tiene que tener
+    Login
+    LoginGuardar(U, P)
+    Registro
+    RegistroGuardar(Nombre, apellido, etc) -> tiene todos los 
+        datos del usuario para después guardarlos
+*/
---
+++ Models/BD.cs
@@ -0,0 +1,14 @@
+
+    /*
+    Tiene que tener
+        Login (usuario, contraseña) -> tipo usuario
+        Registro (usuario) -> void
+        Agregar tarea(tarea) -> void
+        Modificar tarea (tarea) -> void
+        Eliminar tarea (id de tarea) -> void
+        Ver tarea(id de tarea) -> tarea
+        Ver tareas(id de usuario) -> List<Tarea> -> devuelve la 
+            lista de todas las tareas de un usuario
+        Finalizar tarea (id de tarea) -> void -> cambia el bool 
+            de finalizada a true
+    */
