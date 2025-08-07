4715b97af673172c56724823df58bfa764106f93
Flor <49628016@est.ort.edu.ar>
2025-08-07 13:51:28 -0300

    Creamos las models usuario y tarea

---
+++ Models/Tarea.cs
@@ -0,0 +1,11 @@
+namespace TP06.Models;
+
+public class Tarea
+{
+    public string Nombre {get; set;}
+    public string Apellido {get;set;}
+    public string Password {get;set;}
+    public string Foto {get;set;}
+    public DateTime FechaUL {get;set;}
+    public int ID {get;set;}
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,11 @@
+namespace TP06.Models;
+
+public class Usuario
+{
+    public string Titulo {get; set;}
+    public string Descripcion {get;set;}
+    public bool Finalizada {get;set;}
+    public DateTime Fecha {get;set;}
+    public int IdUsuario {get;set;}
+    public int ID {get;set;}
+}
