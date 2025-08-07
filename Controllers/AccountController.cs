c4fb3ba3e65f03383255e13f505ce9472f00a56b
Flor <49628016@est.ort.edu.ar>
2025-08-07 14:56:16 -0300

    Agregue el login en la model BD)

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,8 +1,21 @@
-/*
-Tiene que tener
-    Login
-    LoginGuardar(U, P)
-    Registro
-    RegistroGuardar(Nombre, apellido, etc) -> tiene todos los 
-        datos del usuario para después guardarlos
-*/
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TP06.Models;
+
+namespace TP06.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly ILogger<AccountController> _logger;
+
+    public AccountController(ILogger<AccountController> logger)
+    {
+        _logger = logger;
+    }
+    public IActionResult Index(){
+        return View();
+    }
+    public IActionResult Login(){
+        int 
+    }
+}
--- Models/BD.cs
+++ Models/BD.cs
@@ -12,3 +12,43 @@
         Finalizar tarea (id de tarea) -> void -> cambia el bool 
             de finalizada a true
     */
+
+namespace TP06.Models;
+using Microsoft.Data.SqlClient;
+using Dapper;
+
+public static class BD
+{
+    private static string _connectionString = @"Server=localhost; DataBase=TP06BD; Integrated Security=True; TrustServerCertificate=True;";
+
+    public static Usuario Login(string Usuario, string Password){
+        int Id = 0;
+        using(SqlConnection connection = new SqlConnection(_connectionString)){
+            string query = "SELECT Id FROM Usuario WHERE Usuario = @Usuario AND Password = @Password";
+            Id = connection.QueryFirstOrDefault<int>(query, new { Usuario,  Password});
+        }
+        Usuario usuario = null;
+        if(Id != 0){
+            usuario = GetUsuario(Id);
+            ActualizarFecha(Id);
+        }
+        return usuario;
+    }
+    private static Usuario GetUsuario(int id){
+        Usuario usuario = null;
+        using(SqlConnection connection = new SqlConnection(_connectionString)){
+            string query = "SELECT * FROM Usuario WHERE ID = @Id";
+            usuario = connection.QueryFirstOrDefault<Usuario>(query, new {id});
+        }
+        return usuario;
+    }
+    private static void ActualizarFecha(int ID){
+        using(SqlConnection connection = new SqlConnection(_connectionString)){
+            string query = "UPDATE [Usuario] SET [FechaUL] = GETDATE() WHERE ID = @ID";
+             connection.Execute(query, new {ID});
+        }
+    }
+    
+
+
+}
